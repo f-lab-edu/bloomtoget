01dbdbd1e077319f3ade09db5e416d953f4d1d3b
JBGeum <gysophila001@gmail.com>
2025-10-23 17:15:31 +0900

    초기 설정

---
+++ app/src/main/java/com/btg/BloomtogetApplication.java
@@ -0,0 +1,11 @@
+package com.btg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomtogetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BloomtogetApplication.class, args);
+    }
+}
